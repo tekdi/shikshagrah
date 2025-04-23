64ebc25030349be0fc5d7b598184bbbc7fa0b266
siddhishinde0723 <72926048+siddhishinde0723@users.noreply.github.com>
2025-04-23 18:59:54 +0530

    Update middleware.ts

--- apps/shikshagraha-app/src/middleware.ts
+++ apps/shikshagraha-app/src/middleware.ts
@@ -3,19 +3,19 @@ import { NextResponse } from 'next/server';
 export function middleware(request: { nextUrl: { clone: () => any } }) {
   const url = request.nextUrl.clone();
 
-  if (url.pathname.startsWith('/shikshagraha/registration')) {
+  if (url.pathname.startsWith('/shikshalokam/registration')) {
     url.hostname = 'localhost';
     url.port = '4300';
     return NextResponse.rewrite(url);
   }
 
-  if (url.pathname.startsWith('/shikshagraha/content')) {
+  if (url.pathname.startsWith('/shikshalokam/content')) {
     url.hostname = 'localhost';
     url.port = '4301';
     return NextResponse.rewrite(url);
   }
 
-  if (url.pathname.startsWith('/shikshagraha/pwa')) {
+  if (url.pathname.startsWith('/shikshalokam/pwa')) {
     url.hostname = 'localhost';
     url.port = '4200';
     return NextResponse.rewrite(url);
