60c694feefc46e67d0ba5978b2edfc61ce509b0e
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2025-04-29 12:37:49 +0530

    auth guard added

--- apps/shikshagraha-app/src/app/home/page.tsx
+++ apps/shikshagraha-app/src/app/home/page.tsx
@@ -29,6 +29,7 @@ export default function Home() {
     { title: 'Reports', icon: '/shikshalokam/assets/images/ic_report.png' },
     { title: 'Observation', icon: '/shikshalokam/assets/images/ic_report.png' },
   ];
+  const isAuthenticated = !!localStorage.getItem('accToken');
 
   const router = useRouter();
   const [profileData, setProfileData] = useState(null);
@@ -68,125 +69,131 @@ export default function Home() {
     router.push(`/program?title=${encodeURIComponent(title)}`);
   };
 
-  return (
-    <>
-      <Layout
-        showTopAppBar={{
-          title: 'Home',
-          showMenuIcon: true,
-          showBackIcon: false,
-        }}
-        isFooter={true}
-        showLogo={true}
-        showBack={true}
-      >
-        <Box
-          sx={{
-            minHeight: '100vh',
-            marginTop: { xs: '60px', sm: '90px' },
-            paddingX: { xs: 2, sm: 3 },
+  if(isAuthenticated) {
+    return (
+      <>
+        <Layout
+          showTopAppBar={{
+            title: 'Home',
+            showMenuIcon: true,
+            showBackIcon: false,
           }}
+          isFooter={true}
+          showLogo={true}
+          showBack={true}
         >
-          {loading ? (
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '50vh',
-              }}
-            >
-              <CircularProgress />
-            </Box>
-          ) : error ? (
-            <Typography variant="h6" color="error" textAlign="center">
-              {error}
-            </Typography>
-          ) : (
-            <>
-              <Box sx={{ textAlign: 'center', mb: 4 }}>
-                <Typography
-                  variant="h5"
-                  color="#582E92"
-                  fontWeight="bold"
-                  fontSize={{ xs: '22px', sm: '24px', md: '26px' }}
-                >
-                  Welcome, {profileData?.firstName}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  sx={{ mt: 1, fontSize: { xs: '16px', sm: '20px' } }}
-                >
-                  Browse Shikshagraha library to find relevant content based on
-                  your preferences (Board, Medium, Class, Subject)
-                </Typography>
-              </Box>
-
+          <Box
+            sx={{
+              minHeight: '100vh',
+              marginTop: { xs: '60px', sm: '90px' },
+              paddingX: { xs: 2, sm: 3 },
+            }}
+          >
+            {loading ? (
               <Box
                 sx={{
                   display: 'flex',
-                  gap: 3,
-                  flexWrap: 'wrap',
                   justifyContent: 'center',
+                  alignItems: 'center',
+                  minHeight: '50vh',
                 }}
               >
-                {cardData
-                  .filter((card) => {
-                    const storedHeaders = JSON.parse(
-                      localStorage.getItem('headers') ?? '{}'
-                    ); // Parse the JSON
-                    const storedOrgId = storedHeaders['org-id']; // Get org-id
-                    const isSameOrg =
-                      storedOrgId === process.env.NEXT_PUBLIC_ORGID;
-                    console.log(isSameOrg);
-                    console.log(storedOrgId);
-
-                    return isSameOrg
-                      ? true // Show only these if org ID matches
-                      : card.title === 'Projects' || card.title === 'Reports'; // Show all cards if org ID is different
-                  })
-                  .map((card, index) => (
-                    <DynamicCard
-                      key={index}
-                      title={card.title}
-                      icon={card.icon}
-                      sx={{
-                        borderRadius: 2,
-                        boxShadow: 3,
-                        transition: 'all 0.3s ease',
-                        '&:hover': {
-                          transform: 'scale(1.05)',
-                          boxShadow: 6,
-                        },
-                        maxWidth: { xs: 280, sm: 350 },
-                      }}
-                      onClick={() => handleCardClick(card.title)}
-                    />
-                  ))}
+                <CircularProgress />
               </Box>
-            </>
-          )}
-        </Box>
-      </Layout>
+            ) : error ? (
+              <Typography variant="h6" color="error" textAlign="center">
+                {error}
+              </Typography>
+            ) : (
+              <>
+                <Box sx={{ textAlign: 'center', mb: 4 }}>
+                  <Typography
+                    variant="h5"
+                    color="#582E92"
+                    fontWeight="bold"
+                    fontSize={{ xs: '22px', sm: '24px', md: '26px' }}
+                  >
+                    Welcome, {profileData?.firstName}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{ mt: 1, fontSize: { xs: '16px', sm: '20px' } }}
+                  >
+                    Browse Shikshagraha library to find relevant content based on
+                    your preferences (Board, Medium, Class, Subject)
+                  </Typography>
+                </Box>
 
-      {/* Logout Confirmation Popup */}
-      <Dialog open={showLogoutModal} onClose={handleLogoutCancel}>
-        <DialogTitle>Confirm Logout</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to log out?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleLogoutCancel} color="primary">
-            No
-          </Button>
-          <Button onClick={handleLogoutConfirm} color="secondary">
-            Yes, Logout
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
+                <Box
+                  sx={{
+                    display: 'flex',
+                    gap: 3,
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {cardData
+                    .filter((card) => {
+                      const storedHeaders = JSON.parse(
+                        localStorage.getItem('headers') ?? '{}'
+                      ); // Parse the JSON
+                      const storedOrgId = storedHeaders['org-id']; // Get org-id
+                      const isSameOrg =
+                        storedOrgId === process.env.NEXT_PUBLIC_ORGID;
+                      console.log(isSameOrg);
+                      console.log(storedOrgId);
+
+                      return isSameOrg
+                        ? true // Show only these if org ID matches
+                        : card.title === 'Projects' || card.title === 'Reports'; // Show all cards if org ID is different
+                    })
+                    .map((card, index) => (
+                      <DynamicCard
+                        key={index}
+                        title={card.title}
+                        icon={card.icon}
+                        sx={{
+                          borderRadius: 2,
+                          boxShadow: 3,
+                          transition: 'all 0.3s ease',
+                          '&:hover': {
+                            transform: 'scale(1.05)',
+                            boxShadow: 6,
+                          },
+                          maxWidth: { xs: 280, sm: 350 },
+                        }}
+                        onClick={() => handleCardClick(card.title)}
+                      />
+                    ))}
+                </Box>
+              </>
+            )}
+          </Box>
+        </Layout>
+
+        {/* Logout Confirmation Popup */}
+        <Dialog open={showLogoutModal} onClose={handleLogoutCancel}>
+          <DialogTitle>Confirm Logout</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to log out?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleLogoutCancel} color="primary">
+              No
+            </Button>
+            <Button onClick={handleLogoutConfirm} color="secondary">
+              Yes, Logout
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
+  }
+  else {
+    localStorage.clear();
+    router.push(`${process.env.NEXT_PUBLIC_LOGINPAGE}`);
+  }
 }
