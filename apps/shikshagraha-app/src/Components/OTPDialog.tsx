11641a17f21bdd587ce98187b214e20728b152cf
namita-25 <namita_g@tekditechnologies.com>
2025-05-07 16:48:10 +0530

    code expire in time

--- apps/shikshagraha-app/src/Components/OTPDialog.tsx
+++ apps/shikshagraha-app/src/Components/OTPDialog.tsx
@@ -125,6 +125,7 @@ const OTPDialog: React.FC<OTPDialogProps> = ({
     if (onResendOtp) {
       onResendOtp();
       setResendTimer(resendCooldown);
+      setExpirationTimer(expirationTime);
     }
   };
 
